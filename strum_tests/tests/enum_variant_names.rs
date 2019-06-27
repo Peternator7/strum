0da0a1aef310d994b17388203e8afae6ef4537c2
Pascal Hertleif <pascal@technocreatives.com>
2019-06-27 21:03:27 +0200

    Add test case using `variants` in clap/structopt

--- strum_tests/tests/enum_variant_names.rs
+++ strum_tests/tests/enum_variant_names.rs
@@ -1,5 +1,8 @@
 #[macro_use]
 extern crate strum_macros;
+#[macro_use]
+extern crate structopt;
+extern crate strum;
 
 #[test]
 fn simple() {
@@ -48,4 +51,40 @@ fn non_plain_camel() {
         &Color::variants(),
         &["deep-pink", "green-yellow", "cornflower-blue", "other"]
     );
+}
+
+#[test]
+fn clap_and_structopt() {
+    #[derive(Debug, EnumString, EnumVariantNames)]
+    #[strum(serialize_all = "kebab_case")]
+    enum Color {
+        Red,
+        Blue,
+        Yellow,
+        RebeccaPurple,
+    }
+
+    assert_eq!(
+        &Color::variants(),
+        &["red", "blue", "yellow", "rebecca-purple"]
+    );
+
+    let _clap_example = clap::App::new("app").arg(
+        clap::Arg::with_name("color")
+            .long("color")
+            .possible_values(Color::variants())
+            .case_insensitive(true),
+    );
+
+    #[derive(Debug, StructOpt)]
+    #[allow(unused)]
+    struct StructOptExample {
+        /// The main color
+        #[structopt(
+            long = "color",
+            default_value = "Color::Blue",
+            raw(possible_values = "Color::variants()")
+        )]
+        color: Color,
+    }
 }
