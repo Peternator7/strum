109fd74d7d2ca3fe365e14a6141748b4d95dfcc4
Evgeniy A. Dushistov <dushistov@mail.ru>
2019-07-26 02:59:10 +0300

    applying rustfmt 1.2.2-stable

--- strum_macros/src/lib.rs
+++ strum_macros/src/lib.rs
@@ -22,10 +22,10 @@ mod case_style;
 mod display;
 mod enum_count;
 mod enum_discriminants;
-mod enum_variant_names;
 mod enum_iter;
 mod enum_messages;
 mod enum_properties;
+mod enum_variant_names;
 mod from_string;
 mod helpers;
 mod to_string;
@@ -46,8 +46,14 @@ fn debug_print_generated(ast: &syn::DeriveInput, toks: &TokenStream) {
     }
 }
 
-#[cfg_attr(not(feature = "verbose-enumstring-name"), proc_macro_derive(EnumString, attributes(strum)))]
-#[cfg_attr(feature = "verbose-enumstring-name", proc_macro_derive(StrumEnumString, attributes(strum)))]
+#[cfg_attr(
+    not(feature = "verbose-enumstring-name"),
+    proc_macro_derive(EnumString, attributes(strum))
+)]
+#[cfg_attr(
+    feature = "verbose-enumstring-name",
+    proc_macro_derive(StrumEnumString, attributes(strum))
+)]
 pub fn from_string(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
     let ast = syn::parse(input).unwrap();
 
@@ -56,8 +62,14 @@ pub fn from_string(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
     toks.into()
 }
 
-#[cfg_attr(not(feature = "verbose-asrefstr-name"), proc_macro_derive(AsRefStr, attributes(strum)))]
-#[cfg_attr(feature = "verbose-asrefstr-name", proc_macro_derive(StrumAsRefStr, attributes(strum)))]
+#[cfg_attr(
+    not(feature = "verbose-asrefstr-name"),
+    proc_macro_derive(AsRefStr, attributes(strum))
+)]
+#[cfg_attr(
+    feature = "verbose-asrefstr-name",
+    proc_macro_derive(StrumAsRefStr, attributes(strum))
+)]
 pub fn as_ref_str(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
     let ast = syn::parse(input).unwrap();
 
@@ -66,8 +78,14 @@ pub fn as_ref_str(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
     toks.into()
 }
 
-#[cfg_attr(not(feature = "verbose-variant-names"), proc_macro_derive(EnumVariantNames, attributes(strum)))]
-#[cfg_attr(feature = "verbose-variant-names", proc_macro_derive(StrumEnumVariantNames, attributes(strum)))]
+#[cfg_attr(
+    not(feature = "verbose-variant-names"),
+    proc_macro_derive(EnumVariantNames, attributes(strum))
+)]
+#[cfg_attr(
+    feature = "verbose-variant-names",
+    proc_macro_derive(StrumEnumVariantNames, attributes(strum))
+)]
 pub fn variant_names(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
     let ast = syn::parse(input).unwrap();
 
@@ -76,8 +94,14 @@ pub fn variant_names(input: proc_macro::TokenStream) -> proc_macro::TokenStream 
     toks.into()
 }
 
-#[cfg_attr(feature = "verbose-asstaticstr-name", proc_macro_derive(StrumAsStaticStr, attributes(strum)))]
-#[cfg_attr(not(feature = "verbose-asstaticstr-name"), proc_macro_derive(AsStaticStr, attributes(strum)))]
+#[cfg_attr(
+    feature = "verbose-asstaticstr-name",
+    proc_macro_derive(StrumAsStaticStr, attributes(strum))
+)]
+#[cfg_attr(
+    not(feature = "verbose-asstaticstr-name"),
+    proc_macro_derive(AsStaticStr, attributes(strum))
+)]
 pub fn as_static_str(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
     let ast = syn::parse(input).unwrap();
 
@@ -86,8 +110,14 @@ pub fn as_static_str(input: proc_macro::TokenStream) -> proc_macro::TokenStream 
     toks.into()
 }
 
-#[cfg_attr(feature = "verbose-intostaticstr-name", proc_macro_derive(StrumIntoStaticStr, attributes(strum)))]
-#[cfg_attr(not(feature = "verbose-intostaticstr-name"), proc_macro_derive(IntoStaticStr, attributes(strum)))]
+#[cfg_attr(
+    feature = "verbose-intostaticstr-name",
+    proc_macro_derive(StrumIntoStaticStr, attributes(strum))
+)]
+#[cfg_attr(
+    not(feature = "verbose-intostaticstr-name"),
+    proc_macro_derive(IntoStaticStr, attributes(strum))
+)]
 pub fn into_static_str(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
     let ast = syn::parse(input).unwrap();
 
@@ -96,8 +126,14 @@ pub fn into_static_str(input: proc_macro::TokenStream) -> proc_macro::TokenStrea
     toks.into()
 }
 
-#[cfg_attr(feature = "verbose-tostring-name", proc_macro_derive(StrumToString, attributes(strum)))]
-#[cfg_attr(not(feature = "verbose-tostring-name"), proc_macro_derive(ToString, attributes(strum)))]
+#[cfg_attr(
+    feature = "verbose-tostring-name",
+    proc_macro_derive(StrumToString, attributes(strum))
+)]
+#[cfg_attr(
+    not(feature = "verbose-tostring-name"),
+    proc_macro_derive(ToString, attributes(strum))
+)]
 pub fn to_string(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
     let ast = syn::parse(input).unwrap();
 
@@ -106,8 +142,14 @@ pub fn to_string(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
     toks.into()
 }
 
-#[cfg_attr(feature = "verbose-display-name", proc_macro_derive(StrumDisplay, attributes(strum)))]
-#[cfg_attr(not(feature = "verbose-display-name"), proc_macro_derive(Display, attributes(strum)))]
+#[cfg_attr(
+    feature = "verbose-display-name",
+    proc_macro_derive(StrumDisplay, attributes(strum))
+)]
+#[cfg_attr(
+    not(feature = "verbose-display-name"),
+    proc_macro_derive(Display, attributes(strum))
+)]
 pub fn display(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
     let ast = syn::parse(input).unwrap();
 
@@ -116,8 +158,14 @@ pub fn display(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
     toks.into()
 }
 
-#[cfg_attr(feature = "verbose-enumiter-name", proc_macro_derive(StrumEnumIter, attributes(strum)))]
-#[cfg_attr(not(feature = "verbose-enumiter-name"), proc_macro_derive(EnumIter, attributes(strum)))]
+#[cfg_attr(
+    feature = "verbose-enumiter-name",
+    proc_macro_derive(StrumEnumIter, attributes(strum))
+)]
+#[cfg_attr(
+    not(feature = "verbose-enumiter-name"),
+    proc_macro_derive(EnumIter, attributes(strum))
+)]
 pub fn enum_iter(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
     let ast = syn::parse(input).unwrap();
 
@@ -126,8 +174,14 @@ pub fn enum_iter(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
     toks.into()
 }
 
-#[cfg_attr(feature = "verbose-enummessage-name", proc_macro_derive(StrumEnumMessage, attributes(strum)))]
-#[cfg_attr(not(feature = "verbose-enummessage-name"), proc_macro_derive(EnumMessage, attributes(strum)))]
+#[cfg_attr(
+    feature = "verbose-enummessage-name",
+    proc_macro_derive(StrumEnumMessage, attributes(strum))
+)]
+#[cfg_attr(
+    not(feature = "verbose-enummessage-name"),
+    proc_macro_derive(EnumMessage, attributes(strum))
+)]
 pub fn enum_messages(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
     let ast = syn::parse(input).unwrap();
 
@@ -136,8 +190,14 @@ pub fn enum_messages(input: proc_macro::TokenStream) -> proc_macro::TokenStream 
     toks.into()
 }
 
-#[cfg_attr(feature = "verbose-enumproperty-name", proc_macro_derive(StrumEnumProperty, attributes(strum)))]
-#[cfg_attr(not(feature = "verbose-enumproperty-name"), proc_macro_derive(EnumProperty, attributes(strum)))]
+#[cfg_attr(
+    feature = "verbose-enumproperty-name",
+    proc_macro_derive(StrumEnumProperty, attributes(strum))
+)]
+#[cfg_attr(
+    not(feature = "verbose-enumproperty-name"),
+    proc_macro_derive(EnumProperty, attributes(strum))
+)]
 pub fn enum_properties(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
     let ast = syn::parse(input).unwrap();
 
@@ -146,8 +206,14 @@ pub fn enum_properties(input: proc_macro::TokenStream) -> proc_macro::TokenStrea
     toks.into()
 }
 
-#[cfg_attr(feature = "verbose-enumdiscriminants-name", proc_macro_derive(StrumEnumDiscriminants, attributes(strum, strum_discriminants)))]
-#[cfg_attr(not(feature = "verbose-enumdiscriminants-name"), proc_macro_derive(EnumDiscriminants, attributes(strum, strum_discriminants)))]
+#[cfg_attr(
+    feature = "verbose-enumdiscriminants-name",
+    proc_macro_derive(StrumEnumDiscriminants, attributes(strum, strum_discriminants))
+)]
+#[cfg_attr(
+    not(feature = "verbose-enumdiscriminants-name"),
+    proc_macro_derive(EnumDiscriminants, attributes(strum, strum_discriminants))
+)]
 pub fn enum_discriminants(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
     let ast = syn::parse(input).unwrap();
 
@@ -156,8 +222,14 @@ pub fn enum_discriminants(input: proc_macro::TokenStream) -> proc_macro::TokenSt
     toks.into()
 }
 
-#[cfg_attr(feature = "verbose-enumcount-name", proc_macro_derive(StrumEnumCount, attributes(strum)))]
-#[cfg_attr(not(feature = "verbose-enumcount-name"), proc_macro_derive(EnumCount, attributes(strum)))]
+#[cfg_attr(
+    feature = "verbose-enumcount-name",
+    proc_macro_derive(StrumEnumCount, attributes(strum))
+)]
+#[cfg_attr(
+    not(feature = "verbose-enumcount-name"),
+    proc_macro_derive(EnumCount, attributes(strum))
+)]
 pub fn enum_count(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
     let ast = syn::parse(input).unwrap();
     let toks = enum_count::enum_count_inner(&ast);
