26d888596e57760b342af30a2dac8d1798f8c4b3
Evgeniy A. Dushistov <dushistov@mail.ru>
2019-07-26 03:45:55 +0300

    handling of all options listed in serde docs for compatibility
    See https://serde.rs/container-attrs.html#container-attributes

--- strum_macros/src/case_style.rs
+++ strum_macros/src/case_style.rs
@@ -7,18 +7,26 @@ pub enum CaseStyle {
     SnakeCase,
     TitleCase,
     UpperCase,
+    LowerCase,
+    ScreamingKebabCase,
+    PascalCase,
 }
 
 impl<'s> From<&'s str> for CaseStyle {
     fn from(text: &'s str) -> CaseStyle {
         match text {
-            "camel_case" => CaseStyle::CamelCase,
-            "kebab_case" => CaseStyle::KebabCase,
+            "lowercase" => CaseStyle::LowerCase,
+            "camel_case" | "PascalCase" => CaseStyle::PascalCase,
+            "kebab_case" | "kebab-case" => CaseStyle::KebabCase,
             "mixed_case" => CaseStyle::MixedCase,
-            "shouty_snake_case" | "shouty_snek_case" => CaseStyle::ShoutySnakeCase,
+            "shouty_snake_case" | "shouty_snek_case" | "SCREAMING_SNAKE_CASE" => {
+                CaseStyle::ShoutySnakeCase
+            }
             "snake_case" | "snek_case" => CaseStyle::SnakeCase,
             "title_case" => CaseStyle::TitleCase,
             "UPPERCASE" => CaseStyle::UpperCase,
+            "camelCase" => CaseStyle::CamelCase,
+            "SCREAMING-KEBAB-CASE" => CaseStyle::ScreamingKebabCase,
             _ => panic!(
                 "Unexpected case style for serialize_all: `{}`. Valid values are: `{:?}`",
                 text,
