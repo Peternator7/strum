36e01fc11b576566b8354b6b17fc2b034c8740e1
YOSHIOKA Takuma <lo48576@hard-wi.red>
2019-08-23 00:34:50 +0900

    Fix `clippy::match_ref_pats` lint

--- strum/src/lib.rs
+++ strum/src/lib.rs
@@ -59,7 +59,7 @@ impl std::fmt::Display for ParseError {
         // We could use our macro here, but this way we don't take a dependency on the
         // macros crate.
         match self {
-            &ParseError::VariantNotFound => write!(f, "Matching variant not found"),
+            ParseError::VariantNotFound => write!(f, "Matching variant not found"),
         }
     }
 }
@@ -67,7 +67,7 @@ impl std::fmt::Display for ParseError {
 impl std::error::Error for ParseError {
     fn description(&self) -> &str {
         match self {
-            &ParseError::VariantNotFound => {
+            ParseError::VariantNotFound => {
                 "Unable to find a variant of the given enum matching the string given. Matching \
                  can be extended with the Serialize attribute and is case sensitive."
             }
