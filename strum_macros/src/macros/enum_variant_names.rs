8da192950365221a7717e778104ee3e9b31d2810
Waffle <wafflewafflerov@gmail.com>
2019-11-07 02:11:37 +0300

    Add implementing const `VARIANTS` by `EnumVariantsNames`, deprecate old `::variants`

--- strum_macros/src/macros/enum_variant_names.rs
+++ strum_macros/src/macros/enum_variant_names.rs
@@ -1,4 +1,4 @@
-use proc_macro2::TokenStream;
+use proc_macro2::{TokenStream, Span};
 use syn;
 
 use crate::helpers::case_style::CaseStyle;
@@ -23,14 +23,22 @@ pub fn enum_variant_names_inner(ast: &syn::DeriveInput) -> TokenStream {
         .map(|v| v.ident.convert_case(case_style))
         .collect::<Vec<_>>();
 
+    let note = syn::Lit::Str(syn::LitStr::new(
+        &format!("Use {name}::VARIANTS instead", name = name.to_string()),
+        Span::call_site(),
+    ));
+
     quote! {
         impl #name {
+            /// Names of the variants of this enum
+            #[allow(dead_code)]
+            pub const VARIANTS: &'static [&'static str] = &[ #(#names),* ];
+
             /// Return a slice containing the names of the variants of this enum
             #[allow(dead_code)]
-            pub fn variants() -> &'static [&'static str] {
-                &[
-                    #(#names),*
-                ]
+            #[deprecated = #note]
+            pub const fn variants() -> &'static [&'static str] {
+                Self::VARIANTS
             }
         }
     }
