9c4ca279747ddde44e309e880c79ac4f7d712416
Peter Glotfelty 🚀 <peglotfe@microsoft.com>
2020-09-17 16:15:19 +0000

    Enable travis

--- travis.sh
+++ travis.sh
@@ -1,15 +1,1 @@
-cd ./strum
-cargo build --verbose
 cargo test --verbose
-
-cd ..
-cd ./strum_tests
-cargo build --verbose
-cargo test --verbose
-
-cd ..
-cd ./strum_tests_rename
-cargo build --verbose
-cargo test --verbose
-
-cd ..
