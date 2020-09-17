4446879efaa63f1d2b264bdc97b0fbc65ec3ae3b
Peter Glotfelty 🚀 <peglotfe@microsoft.com>
2020-09-17 16:15:43 +0000

    add build.r to re-enable try_from

---
+++ strum_tests/build.rs
@@ -0,0 +1,7 @@
+
+fn main() {
+    // Check if version of rustc is >= 1.34
+    if let Some(true) = version_check::is_min_version("1.34.0") {
+        println!("cargo:rustc-cfg=try_from");
+    }
+}
