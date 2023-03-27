2ec3c6f370c39e3fb5d3d597aab6697a8efffed1
MendyBerger <12537668+MendyBerger@users.noreply.github.com>
2023-03-26 21:07:07 -0400

    feat(is): Use a simple match and cover all cases

--- strum_macros/src/macros/enum_is.rs
+++ strum_macros/src/macros/enum_is.rs
@@ -17,34 +17,13 @@ pub fn enum_is_inner(ast: &DeriveInput) -> syn::Result<TokenStream> {
         .map(|variant| {
             let variant_name = &variant.ident;
             let fn_name = format_ident!("is_{}", variant_name.to_string().to_case(Case::Snake));
-
-            match &variant.fields {
-                syn::Fields::Named(_) => {
-                    quote! {
-                        #[must_use]
-                        #[inline]
-                        pub const fn #fn_name(&self) -> bool {
-                            matches!(self, &#enum_name::#variant_name { .. })
-                        }
-                    }
-                }
-                syn::Fields::Unnamed(values) => {
-                    let underscores = values.unnamed.iter().map(|_| quote! {_});
-                    quote! {
-                        #[must_use]
-                        #[inline]
-                        pub const fn #fn_name(&self) -> bool {
-                            matches!(self, &#enum_name::#variant_name (#(#underscores),*))
-                        }
-                    }
-                }
-                syn::Fields::Unit => {
-                    quote! {
-                        #[must_use]
-                        #[inline]
-                        pub const fn #fn_name(&self) -> bool {
-                            matches!(self, &#enum_name::#variant_name)
-                        }
+            quote! {
+                #[must_use]
+                #[inline]
+                pub const fn #fn_name(&self) -> bool {
+                    match self {
+                        &#enum_name::#variant_name { .. } => true,
+                        _ => false
                     }
                 }
             }
