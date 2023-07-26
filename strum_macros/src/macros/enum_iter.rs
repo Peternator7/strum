1e0a1f32b8982e7a99e3a40f50386119cad0fdd6
kraktus <kraktus@users.noreply.github.com>
2023-07-26 22:57:59 +0200

    Fix macro of `EnumIter`
    close https://github.com/Peternator7/strum/issues/284

--- strum_macros/src/macros/enum_iter.rs
+++ strum_macros/src/macros/enum_iter.rs
@@ -80,8 +80,8 @@ pub fn enum_iter_inner(ast: &DeriveInput) -> syn::Result<TokenStream> {
             marker: ::core::marker::PhantomData #phantom_data,
         }
 
-        impl #impl_generics core::fmt::Debug for #iter_name #ty_generics #where_clause {
-            fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+        impl #impl_generics ::core::fmt::Debug for #iter_name #ty_generics #where_clause {
+            fn fmt(&self, f: &mut ::core::fmt::Formatter<'_>) -> ::core::fmt::Result {
                 // We don't know if the variants implement debug themselves so the only thing we
                 // can really show is how many elements are left.
                 f.debug_struct(#iter_name_debug_struct)
