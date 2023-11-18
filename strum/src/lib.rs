09dbfaf3b8cb533bbe24db02c6f5de5017de4b41
Brenno Lemos <brenno@syndel.is>
2023-11-18 02:41:33 -0300

    docs: include documentation for trait and derive of EnumDeref

--- strum/src/lib.rs
+++ strum/src/lib.rs
@@ -206,7 +206,9 @@ pub trait EnumCount {
     const COUNT: usize;
 }
 
-/// TODO: Doc this
+/// A trait for reducing boilerplate code related to common access patterns in
+/// new-type-like variant enums. Because of this, for derived usage, all the
+/// variants in the enumerator need to be new-types (e.g. Int(i32), Person(Person))
 pub trait EnumDeref: std::ops::Deref + std::ops::DerefMut {}
 
 impl<T: std::ops::Deref + std::ops::DerefMut> EnumDeref for T {}
--- strum_macros/src/lib.rs
+++ strum_macros/src/lib.rs
@@ -891,7 +891,75 @@ pub fn enum_count(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
     toks.into()
 }
 
-/// TODO: Doc this
+/// A trait for grouping together different new-type variants that dereference to
+/// the same type, reducing boilerplate code.
+/// 
+/// Implements [`std::ops::Deref`] and [`std::ops::DerefMut`] for enums composed
+/// of  solely of new-type-style variants.
+/// 
+/// The variants must hold "compatible" types, which means they must be deref-able
+/// to the same type (e.g. String and &str, Box<T> and T).
+/// 
+/// The macro accompanies the macro-attribute `strum_deref_target` to define what type the enum should
+/// deref to.
+/// 
+/// ```
+/// use strum::EnumDeref;
+/// use strum_macros::EnumDeref;
+/// 
+/// #[derive(EnumDeref)]
+/// #[strum_deref_target([u32])]
+/// enum Collection {
+///     Fixed([u32; 2]),
+///     Dynamic(Vec<u32>),
+/// }
+/// 
+/// let mut f = Collection::Fixed([5, 6]);
+/// assert_eq!(&*f, &[5, 6]);
+/// 
+/// f[0] = 10;
+/// assert_eq!(&*f, &[10, 6]);
+/// 
+/// let mut d = Collection::Dynamic(Vec::from([9]));
+/// assert_eq!(&*d, &[9]);
+/// 
+/// d[0] = 1;
+/// assert_eq!(&*d, &[1]);
+/// ```
+/// 
+/// You may also deref trait object references using `dyn Trait` for something
+/// resembling a `Box<T>`, but with all the benefits of having an enum
+/// (compile-time known variants, possibility of using other strum macros).
+///
+/// ```
+/// use strum::EnumDeref;
+/// use strum_macros::EnumDeref;
+/// 
+/// #[derive(EnumDeref)]
+/// #[strum_deref_target(dyn core::fmt::Display)]
+/// enum Column {
+///     Text(String),
+///     Number(i32),
+///     Float(f32),
+/// }
+/// 
+/// let t = Column::Text("Hi!".to_string());
+/// assert_eq!((*t).to_string(), "Hi!");
+/// 
+/// let n = Column::Number(4);
+/// assert_eq!((*n).to_string(), "4");
+/// 
+/// let f = Column::Float(5.125);
+/// assert_eq!((*f).to_string(), "5.125");
+/// 
+/// fn prints_something(smth: &dyn core::fmt::Display) {
+///     println!("{smth}");
+/// }
+/// 
+/// prints_something(&*t);
+/// prints_something(&*n);
+/// prints_something(&*f);
+/// ```
 #[proc_macro_derive(EnumDeref, attributes(strum, strum_deref_target))]
 pub fn enum_deref(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
     let ast = syn::parse_macro_input!(input as DeriveInput);
