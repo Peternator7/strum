2cb311f44b95a8e8143949d11877bc28b10b54b1
Bogdan Mircea <mirceapetrebogdan@gmail.com>
2024-02-04 15:11:23 +0200

    Added handling of the transparent attribute

--- strum_macros/src/macros/strings/as_ref_str.rs
+++ strum_macros/src/macros/strings/as_ref_str.rs
@@ -2,7 +2,9 @@ use proc_macro2::TokenStream;
 use quote::quote;
 use syn::{parse_quote, Data, DeriveInput, Fields};
 
-use crate::helpers::{non_enum_error, HasStrumVariantProperties, HasTypeProperties};
+use crate::helpers::{
+    non_enum_error, non_single_field_variant_error, HasStrumVariantProperties, HasTypeProperties,
+};
 
 fn get_arms(ast: &DeriveInput) -> syn::Result<Vec<TokenStream>> {
     let name = &ast.ident;
@@ -22,17 +24,34 @@ fn get_arms(ast: &DeriveInput) -> syn::Result<Vec<TokenStream>> {
             continue;
         }
 
-        // Look at all the serialize attributes.
-        // Use `to_string` attribute (not `as_ref_str` or something) to keep things consistent
-        // (i.e. always `enum.as_ref().to_string() == enum.to_string()`).
-        let output = variant_properties.get_preferred_name(type_properties.case_style);
-        let params = match variant.fields {
-            Fields::Unit => quote! {},
-            Fields::Unnamed(..) => quote! { (..) },
-            Fields::Named(..) => quote! { {..} },
+        let arm = if variant_properties.transparent.is_some() {
+            let arm_end = match &variant.fields {
+                Fields::Unnamed(f) if f.unnamed.len() == 1 => {
+                    quote! { (ref v) => ::core::convert::AsRef::<str>::as_ref(v) }
+                }
+                Fields::Named(f) if f.named.len() == 1 => {
+                    let ident = f.named.last().unwrap().ident.as_ref().unwrap();
+                    quote! { {ref #ident} => ::core::convert::AsRef::<str>::as_ref(#ident) }
+                }
+                _ => return Err(non_single_field_variant_error()),
+            };
+
+            quote! { #name::#ident #arm_end }
+        } else {
+            // Look at all the serialize attributes.
+            // Use `to_string` attribute (not `as_ref_str` or something) to keep things consistent
+            // (i.e. always `enum.as_ref().to_string() == enum.to_string()`).
+            let output = variant_properties.get_preferred_name(type_properties.case_style);
+            let params = match variant.fields {
+                Fields::Unit => quote! {},
+                Fields::Unnamed(..) => quote! { (..) },
+                Fields::Named(..) => quote! { {..} },
+            };
+
+            quote! { #name::#ident #params => #output }
         };
 
-        arms.push(quote! { #name::#ident #params => #output });
+        arms.push(arm);
     }
 
     if arms.len() < variants.len() {
