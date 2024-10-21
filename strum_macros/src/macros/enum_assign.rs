f0703979213ad74d13219405f0397b8f1278554f
SCDevel <ryanpopp2003@gmail.com>
2024-10-20 17:33:27 -0700

    I think it's time for some chicken dinner!!!!

--- strum_macros/src/macros/enum_assign.rs
+++ strum_macros/src/macros/enum_assign.rs
@@ -1,7 +1,7 @@
 use std::collections::HashSet;
 use proc_macro2::{TokenStream};
 use quote::{format_ident, quote, ToTokens};
-use syn::{Data, DeriveInput};
+use syn::{Data, DeriveInput, parse_quote};
 use crate::helpers::{HasStrumVariantProperties, HasTypeProperties, non_enum_error};
 
 pub fn enum_assign_inner(ast: &DeriveInput) -> syn::Result<TokenStream> {
@@ -9,7 +9,7 @@ pub fn enum_assign_inner(ast: &DeriveInput) -> syn::Result<TokenStream> {
         Data::Enum(v) => &v.variants,
         _ => return Err(non_enum_error()),
     };
-   // let (impl_generics, ty_generics, where_clause) = ast.generics.split_for_impl();
+   let (impl_generics, ty_generics, where_clause) = ast.generics.split_for_impl();
 
     let type_properties = ast.get_type_properties()?;
 
@@ -20,13 +20,7 @@ pub fn enum_assign_inner(ast: &DeriveInput) -> syn::Result<TokenStream> {
     };
 
     let enum_name = &ast.ident;
-    let enum_name_struct = format_ident!("{}Struct", enum_name);
-    let enum_name_structs = format_ident!("{}Structs", enum_name);
-    let enum_name_structs_all = format_ident!("{}StructsAll", enum_name);
-
-    let mut variant_enums: Vec<_> = Vec::new();
-    let mut variant_structs: Vec<_> = Vec::new();
-    let mut variant_inner_structs: Vec<_> = Vec::new();
+    let enum_name_group = format_ident!("{}Group", enum_name);
 
     let mut all_types: Vec<_> = Vec::new();
 
@@ -34,96 +28,85 @@ pub fn enum_assign_inner(ast: &DeriveInput) -> syn::Result<TokenStream> {
         .iter()
         .filter_map(|variant| {
             if variant.get_variant_properties().ok()?.disabled.is_some() {
-                return None; //TODO: When Disabled use regular enum args instead of nothing.
+                return None;
             }
 
-            let variant_ident = variant.ident.clone();
-
-            let enum_name_variant_struct = format_ident!("{}_{}_STRUCT", enum_name.to_string().to_uppercase(), variant_ident.to_string().to_uppercase());
-            let enum_name_variant_struct_inner = format_ident!("{}_{}_struct", enum_name.to_string().to_lowercase(), variant_ident.to_string().to_lowercase());
-
-            let mut variant_args: Vec<_> = Vec::new();
+            let mut is_named = false;
 
             let mut args: Vec<_> = Vec::new();
             let mut types: Vec<_> = Vec::new();
             for (i, field) in variant.fields.iter().enumerate() {
                 let var;
+
                 if let Some(_var) = field.clone().ident {
-                    var = _var
+                    var = format_ident!("{}", _var);
+                    is_named = true;
                 } else {
                     var = format_ident!("_{}", i);
                 }
                 let fd = field.ty.to_token_stream();
-                args.push(quote! {
-                    pub #var: #fd,
-                });
+                args.push(var);
                 types.push(quote! {
                     #fd
-                })
+                });
             }
             if args.len() > 0 {
-                variant_args.push(quote! {
-                    #(#args)*
-                });
-            }
-
-            if variant_args.len() > 0 {
-                all_types.push(quote! {
+                all_types.push((variant.ident.clone(), args, quote! {
                     (#(#types),*)
-                });
-
-                variant_enums.push(quote! {
-                    #variant_ident(#enum_name_structs::#enum_name_variant_struct),
-                });
-
-                variant_inner_structs.push(quote! {
-                    #enum_name_variant_struct_inner: Option<(#(#types),*)>
-                });
-
-                variant_structs.push(quote! {
-                    #derives
-                    pub struct #enum_name_variant_struct {
-                       #(#variant_args)*
-                    }
-                });
-            } else {
-                variant_enums.push(quote! {
-                    #variant_ident(),
-                });
+                }, is_named));
             }
 
             Some(0)
         })
                 .collect();
 
-    all_types.dedup_by(|t1, t2| {
-        t1.to_string().eq(&t2.to_string())
-    });
-
+    let mut arms: Vec<_> = Vec::new();
+    let mut struct_groups: Vec<_> = Vec::new();
     let mut temp: HashSet<_> = HashSet::new();
-    let all_types: Vec<_> = all_types.iter().filter_map(|ty| {
-        let tys = ty.to_string();
-        let id = format_ident!("{}", tys.replace(&[',', '(', ')', ':', '<', '>', '?', '[', ']', '{', '}', '|'], "").trim().replace(&[' '], "_").to_lowercase());
-        if !temp.insert(tys) {
-            return None;
-        }
+    let groups: Vec<_> = all_types.iter().filter_map(|(ident, args, ty, is_named)| {
+        let tys = ty.to_string().trim().to_lowercase();
+        if !temp.insert(tys.clone()) { return None; }
+        let id = format_ident!("g_{}", tys
+            .replace(&[',', '(', ')', ':', '<', '>', '?', '[', ']', '{', '}', '|'], "")
+            .replace(&[' '], "_")
+            .replace("__", "_"));
+        let ref_args = quote! { (#(#args.clone()),*) };
+        let argz = quote! { #(#args),* };
+        let arm = if is_named.clone() {
+            quote! {
+                #enum_name::#ident {#argz} => { g.#id = Some(#ref_args); }
+            }
+        } else {
+            quote! {
+                #enum_name::#ident (#argz) => { g.#id = Some(#ref_args); }
+            }
+        };
+        arms.push(arm);
+        struct_groups.push(quote! {
+            #id: Option<#ty>,
+        });
         Some(quote! { #id })
     }).collect();
+
     Ok(quote! {
-        #(#all_types)*
 
         #derives
-        pub enum #enum_name_struct {
-            #(#variant_enums)*
+        pub struct #enum_name_group {
+            #(#struct_groups)*
         }
 
-        #derives
-        pub struct #enum_name_structs_all {
-            #(#variant_inner_structs)*
-        }
-
-        pub mod #enum_name_structs {
-            #(#variant_structs)*
+        impl #enum_name {
+            fn groups(&self) -> #enum_name_group {
+                let mut g = #enum_name_group {
+                    #(#groups: None),*
+                };
+                match self {
+                    #(#arms)*
+                    _ => { return g; }
+                }
+                return g;
+            }
         }
     }.into())
 }
+
--- strum_tests/tests/enum_assign.rs
+++ strum_tests/tests/enum_assign.rs
@@ -1,11 +1,8 @@
 use strum_macros::EnumAssign;
-use crate::FooStructs::*;
-
-mod core {} // ensure macros call `::core`
 #[derive(EnumAssign)]
 enum Foo {
-    Unit(String),
-    Named0(String),
+    Unit(),
+    Named0(),
     Named1 {
         _a: char,
     },
@@ -13,40 +10,20 @@ enum Foo {
         _a: u32,
         _b: String,
     },
-    Unnamed0(u32),
+    Unnamed0(),
     Unnamed1(Option<u128>),
     Unnamed2(bool, u8),
-    MultiWordName(String),
+    MultiWordName(),
     #[strum(disabled)]
     #[allow(dead_code)]
-    Disabled,
+    Disabled(bool),
 }
 
 #[test]
 fn test_func() {
+    let n1 = Foo::Named1 { _a: 'a' };
 
-    let n1 = FooStruct::Named1(FOO_NAMED1_STRUCT { _a: '2' });
-    let u2 = FooStruct::Unnamed2(FOO_UNNAMED2_STRUCT { _0: false, _1: 5 });
-
-    println!("n1: {:?}", n1);
-    tes(n1);
-}
-
-fn tes(t: FooStruct) -> FooStructsAll {
-    match t {
-        FooStruct::Named1(t) => {
-            FooStructsAll {
-                foo_unit_struct: None,
-                foo_named0_struct: None,
-                foo_named1_struct: None,
-                foo_named2_struct: None,
-                foo_unnamed0_struct: None,
-                foo_unnamed1_struct: None,
-                foo_unnamed2_struct: None,
-                foo_multiwordname_struct: None,
-            }
-        },
-        _ => {}
+    if let Some(g) = n1.groups().g_char {
+        println!("{}", g);
     }
 }
-
